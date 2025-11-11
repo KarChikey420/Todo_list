8299241eb7f51f4656187b575464e2c4a028d358
KarChikey <kartikeynegi2002@gmail.com>
2025-11-11 14:54:22 +0530

    add

--- app.py
+++ app.py
@@ -7,37 +7,42 @@ import os
 load_dotenv()
 
 app=Flask(__name__)
+CORS(app)
+
 
 def get_connection():
-    conn=get_connection(
+    conn=psycopg2.connect(
         host=os.getenv('host'),
         database=os.getenv('database'),
-        user=os.getenv('postgres'),
+        user=os.getenv('user'),
         password=os.getenv('password')
     )
+    return conn
 
 @app.route('/api/tasks',methods=['GET'])
 def get_task():
     conn=get_connection()
     cur=conn.cursor()
-    cur.execute("Create table if not exists task(id SERIAL PRIMARY KEY,task TEXT NOT NULL,done BOOLEAN DEFAULT FALSE)")
-    cur.execute("Select * from task Order by id ASC")
+    cur.execute("Select * from tasks Order by id ASC")
     tasks=cur.fetchall()
     cur.close()
     conn.close()
     
-    result=[{"id":t[1],"task":t[2],"done":t[3]}
+    result=[{"id":t[0],"task":t[1],"done":t[2]}
             for t in tasks
             ]
-    return jsonify(request)
+    
+    return jsonify(result)
+print("this is done")
 
-app.route('/api/tasks',methods=['POST'])
+@app.route('/api/tasks',methods=['POST'])
 def add_task():
     data=request.get_json()
     task=data.get("task")
     
     conn=get_connection()
     cur=conn.cursor()
+    cur.execute("Create table if not exists tasks(id SERIAL PRIMARY KEY,task TEXT NOT NULL,done BOOLEAN DEFAULT FALSE)")
     cur.execute("INSERT INTO tasks (task) VALUES (%s)",(task,))
     conn.commit()
     cur.close()
@@ -49,18 +54,18 @@ def add_task():
 def complete_task(task_id):
     conn=get_connection()
     cur=conn.cursor()
-    cur.execute("UPDATE task SET done=TURE WHERE id =%s",(task_id))
+    cur.execute("UPDATE tasks SET done=TRUE WHERE id =%s",(task_id,))
     conn.commit()
     cur.close()
     conn.close()
     
     return jsonify({"message":"Task completed"}) 
 
-@app.route('/api/tasks/<int:task_id>',metods=['DELETE'])
+@app.route('/api/tasks/<int:task_id>',methods=['DELETE'])
 def delete_data(task_id):
     conn=get_connection()
     cur=conn.cursor()
-    cur.execute("DELETE FROM task WHERE id=%s",(task_id,))
+    cur.execute("DELETE FROM tasks WHERE id=%s",(task_id,))
     conn.commit()
     cur.close()
     conn.close()
